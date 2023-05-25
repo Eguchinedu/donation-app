6bb9417dbbe9af7976ccb945a6c3917c826500e3
Egu Chinedu <eguchinedu18@gmail.com>
2023-05-25 03:47:07 +0100

    edited donate page

--- src/app/Pages/home/section-one/section-one.component.ts
+++ src/app/Pages/home/section-one/section-one.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-section-one',
   templateUrl: './section-one.component.html',
   styleUrls: ['./section-one.component.css']
 })
-export class SectionOneComponent {
-
+export class SectionOneComponent implements OnInit {
+  constructor(private router: Router) {}
+  ngOnInit(): void {}
+  donatePage() {
+    this.router.navigate(['/donate']);
+  }
 }
