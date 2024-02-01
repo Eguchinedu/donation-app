8e122565139ff89138182cd8fc11b448b11f7ece
Egu Chinedu <eguchinedu18@gmail.com>
2024-02-01 13:41:28 +0100

    added stats

--- src/app/Pages/home/section-four/section-four.component.ts
+++ src/app/Pages/home/section-four/section-four.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface statCards{
+  title : string;
+  stat: number;
+}
+
 @Component({
   selector: 'app-section-four',
   templateUrl: './section-four.component.html',
-  styleUrls: ['./section-four.component.css']
+  styleUrls: ['./section-four.component.css'],
 })
 export class SectionFourComponent {
-
+  statcards: statCards[] = [
+    {
+      title: 'Total Amount Donated',
+      stat: 0,
+    },
+    {
+      title: 'Total Number of Beneficiaries ',
+      stat: 0,
+    },
+    {
+      title: 'Total Number of States reached',
+      stat: 0,
+    },
+    {
+      title: 'Total Number of Countries reached',
+      stat: 0,
+    },
+  ];
 }
